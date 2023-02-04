df327126674b0881b46af98a0785d071469d5df9
Jay Chiu <jaycchiu524@gmail.com>
2023-02-04 17:34:05 -0500

    feat: client add item card with acc api

---
+++ packages/client/src/api/accounts.ts
@@ -0,0 +1,36 @@
+import { AxiosError } from 'axios'
+
+import { ErrorResponse, Account } from '@/api/types'
+
+import { fetcher } from '@/api/fetcher'
+
+export type AccountType = Account
+type GetAccountsByItemIdResponse = AccountType[]
+type GetAccountsByUserIdResponse = AccountType[]
+
+export const getAccountByItemId = async (itemId: string) => {
+  try {
+    const api = await fetcher()
+    if (!api) return
+    const response = await api.get<GetAccountsByItemIdResponse>(
+      `/items/${itemId}/accounts`,
+    )
+    return response
+  } catch (err) {
+    const error = err as AxiosError<ErrorResponse>
+    throw error
+  }
+}
+export const getAccountsByUserId = async (userId: string) => {
+  try {
+    const api = await fetcher()
+    if (!api) return
+    const response = await api.get<GetAccountsByUserIdResponse>(
+      `/users/${userId}/accounts`,
+    )
+    return response
+  } catch (err) {
+    const error = err as AxiosError<ErrorResponse>
+    throw error
+  }
+}
---
+++ packages/client/src/components/ItemCard/ItemCard.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react'
+import List from '@mui/material/List'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Collapse from '@mui/material/Collapse'
+import ExpandLess from '@mui/icons-material/ExpandLess'
+import ExpandMore from '@mui/icons-material/ExpandMore'
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance'
+import SavingsIcon from '@mui/icons-material/Savings'
+import CreditCardIcon from '@mui/icons-material/CreditCard'
+
+import { useQuery } from '@tanstack/react-query'
+
+import { ItemType } from '@/api/items'
+import { getAccountByItemId } from '@/api/accounts'
+
+type Props = {
+  item: ItemType
+}
+
+const ItemCard = ({ item }: Props) => {
+  const [open, setOpen] = React.useState(false)
+
+  const handleClick = React.useCallback(() => {
+    setOpen((prev) => !prev)
+  }, [])
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['itemById', item.id],
+    queryFn: () => getAccountByItemId(item.id),
+    enabled: open,
+    cacheTime: 60 * 1000,
+  })
+
+  const accounts = data?.data || []
+
+  return (
+    <List
+      sx={{ width: '100%' }}
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      // subheader={
+      //   <ListSubheader component="div" id="nested-list-subheader">
+      //     {item.plaidInstitutionName}
+      //   </ListSubheader>
+      // }
+    >
+      <ListItemButton onClick={handleClick}>
+        <ListItemIcon>
+          <AccountBalanceIcon />
+        </ListItemIcon>
+        <ListItemText primary={item.plaidInstitutionName} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          accounts.sort().map((account) => {
+            /**
+             * export declare enum AccountType {
+              Investment = "investment",
+              Credit = "credit",
+              Depository = "depository",
+              Loan = "loan",
+              Brokerage = "brokerage",
+              Other = "other"
+             */
+            const isDepository = account.type === 'depository'
+
+            return (
+              <List key={account.id} component="div" disablePadding>
+                <ListItemButton sx={{ pl: 4 }}>
+                  <ListItemIcon>
+                    {isDepository ? <SavingsIcon /> : <CreditCardIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={account.name} />
+                </ListItemButton>
+              </List>
+            )
+          })
+        )}
+      </Collapse>
+    </List>
+  )
+}
+
+export default ItemCard
