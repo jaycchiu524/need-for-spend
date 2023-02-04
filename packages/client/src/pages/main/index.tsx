46202be70e694796f4e1394b3b4f9bdc9a34bd56
Jay Chiu <jaycchiu524@gmail.com>
2023-02-04 17:46:55 -0500

    feat: main dashboard

--- packages/client/src/pages/main/index.tsx
+++ packages/client/src/pages/main/index.tsx
@@ -1,69 +1,57 @@
-import React, { ReactNode, useEffect } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 
-import { usePlaidLink, PlaidLinkOptions } from 'react-plaid-link'
+import Button from '@mui/material/Button'
 
-import { useMutation } from '@tanstack/react-query'
+import useLink from '@/hooks/useLink'
+
+import useItems from '@/hooks/useItems'
 
 import MainLayout from '@/components/main/MainLayout'
-import { createLinkToken, exchangeAccessToken } from '@/components/Plaid/api'
+import { useItemStore } from '@/store/items'
+import { useLinkTokenStore } from '@/store/link-token'
+import { useAuthStore } from '@/store/auth'
+import LaunchLink from '@/components/LaunchLink'
+import ItemCard from '@/components/ItemCard/ItemCard'
 
 function PlaidHome() {
-  // get a link_token from your API when component mounts
-  const { data: linkToken, mutateAsync: requestLinkToken } = useMutation({
-    mutationKey: ['create-link-token'],
-    mutationFn: createLinkToken,
-  })
+  const userId = useAuthStore.getState().auth?.id
+  const userItem = useItemStore().byUser
+  const { users } = useLinkTokenStore.getState()
+  const { generateLinkToken } = useLink()
+  const { fetchItemsByUser } = useItems({ userId: userId!, itemId: null })
 
-  const { data: item, mutateAsync: requestAccessToken } = useMutation({
-    mutationKey: ['exchange-access-token'],
-    mutationFn: exchangeAccessToken,
-  })
-
-  const token = linkToken?.data.link_token || null
-
-  useEffect(() => {
-    requestLinkToken()
-  }, [])
-
-  // The usePlaidLink hook manages Plaid Link creation
-  // It does not return a destroy function;
-  // instead, on unmount it automatically destroys the Link instance
-  const config: PlaidLinkOptions = {
-    onSuccess: async (public_token, metadata) => {
-      const { institution, accounts } = metadata
-
-      const response = await requestAccessToken({
-        publicToken: public_token,
-        institutionId: institution?.institution_id || '',
-        institutionName: institution?.name || '',
-        accounts,
-      })
-
-      console.log(response)
-    },
-    onExit: (err, metadata) => {},
-    onEvent: (eventName, metadata) => {},
-    token,
-    //required for OAuth; if not using OAuth, set to null or omit:
-    // receivedRedirectUri: window.location.href,
+  const initialToken = async () => {
+    if (!userId) return null
+    await generateLinkToken(userId, null)
   }
 
-  const { open, exit, ready } = usePlaidLink(config)
+  useEffect(() => {
+    if (!userId) return
+    fetchItemsByUser(userId)
+  }, [userId, fetchItemsByUser])
+
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => setMounted(true), [])
+
+  const items = userItem[userId!] || []
+
+  console.log(items)
 
   return (
     <div>
-      <h1>Plaid Demo</h1>
-      <button onClick={() => open()} disabled={!ready}>
+      <Button
+        variant="outlined"
+        onClick={initialToken}
+        disabled={!!(userId && users[userId])}>
         Connect a bank account
-      </button>
+      </Button>
 
-      <pre>
-        <code>{JSON.stringify(linkToken, null, 2)}</code>
-      </pre>
+      {userId && users[userId] && (
+        <LaunchLink userId={userId} itemId={null} linkToken={users[userId]} />
+      )}
 
-      <pre>
-        <code>{JSON.stringify(item, null, 2)}</code>
-      </pre>
+      {mounted && items.map((item) => <ItemCard key={item.id} item={item} />)}
     </div>
   )
 }
