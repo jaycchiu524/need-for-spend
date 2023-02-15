76c8b6264b3228ddce1d8d290d68ca3338a63ddf
Jay Chiu <jaycchiu524@gmail.com>
2023-02-15 16:14:30 -0500

    feat: add animation to piechart

--- packages/client/src/components/PieChart/PieChart.tsx
+++ packages/client/src/components/PieChart/PieChart.tsx
@@ -25,6 +25,38 @@ const PieChart = (props: Props) => {
   const w = width
   const radius = w / 2
 
+  // setting up chart
+  const generator = d3.pie<void, Datum>()
+  const arcs = generator
+    .padAngle(1 / radius)
+    .sort(null)
+    .value((d) => d.value)(data)
+
+  const arcGenerator = d3
+    .arc<d3.PieArcDatum<Datum>>()
+    .innerRadius(radius / 2)
+    .outerRadius(radius)
+  const color = d3.scaleOrdinal<number, string>().range(d3.schemeSet2)
+
+  const sizes = {
+    innerRadius: radius / 2,
+    outerRadius: radius,
+  }
+
+  const durations = {
+    entryAnimation: 2000,
+  }
+
+  // For animating the chart
+  const angleInterpolation = d3.interpolate(
+    generator.startAngle()(data),
+    generator.endAngle()(data),
+  )
+
+  // For expanding the chart
+  const innerRadiusInterpolation = d3.interpolate(0, sizes.innerRadius)
+  const outerRadiusInterpolation = d3.interpolate(0, sizes.outerRadius)
+
   useEffect(() => {
     setData(props.data)
 
@@ -39,18 +71,6 @@ const PieChart = (props: Props) => {
       // .attr('height', w)
       .attr('style', `max-width: 100%; height: auto; height: intrinsic;`)
 
-    // setting up chart
-    const arcs = d3
-      .pie<void, Datum>()
-      .padAngle(1 / radius)
-      .sort(null)
-      .value((d) => d.value)(data)
-    const arcGenerator = d3
-      .arc<d3.PieArcDatum<Datum>>()
-      .innerRadius(radius / 2)
-      .outerRadius(radius)
-    const color = d3.scaleOrdinal<number, string>().range(d3.schemeSet2)
-
     // setting up svg data
     d3.select(pieRef.current)
       // .attr('stroke', 'red')
@@ -61,18 +81,55 @@ const PieChart = (props: Props) => {
       .attr('d', arcGenerator)
       .attr('fill', (d) => color(d.value))
       .style('opacity', 0.7)
+      .transition()
+      .duration(durations.entryAnimation)
+      .attrTween('d', (d) => {
+        const originalEnd = d.endAngle
+        return (t) => {
+          const currentAngle = angleInterpolation(t)
+          if (currentAngle < d.startAngle) {
+            return ''
+          }
+
+          d.endAngle = Math.min(currentAngle, originalEnd)
+
+          return arcGenerator(d) || ''
+        }
+      })
 
     // setting up annotations
     d3.select(pieRef.current)
-      .attr('font-family', 'sans-serif')
-      .attr('font-size', '1vw')
+      .attr('font-family', 'roboto')
+      .attr('font-weight', 'bold')
       .attr('text-anchor', 'middle')
       .selectAll('text')
       .data(arcs)
       .join('text')
       .text((d) => `${d.data.name} (${d.data.value})`)
       .attr('transform', (d) => `translate(${arcGenerator.centroid(d)})`)
+      .transition()
+      .duration(durations.entryAnimation)
+      .attrTween('font-size', (d) => {
+        const fontSize = d3.interpolateNumber(0, 1)
+        return (t) => `${fontSize(t)}vw`
+      })
+    // .tween('text', (d) => {
+    //   const i = d3.interpolateNumber(0, d.endAngle - d.startAngle)
+    //   return (t) => {
+    //     d.endAngle = d.startAngle + i(t)
+    //   }
+    // })
 
+    // expanding chart
+    d3.select(svgRef.current)
+      .transition()
+      .duration(durations.entryAnimation)
+      .tween('arcRadii', () => {
+        return (t: number) =>
+          arcGenerator
+            .innerRadius(innerRadiusInterpolation(t))
+            .outerRadius(outerRadiusInterpolation(t))
+      })
     // setting up legend
     // const legend = svg
     //   .select('g.legend')
@@ -97,12 +154,23 @@ const PieChart = (props: Props) => {
     //   .attr('y', 9.5)
     //   .attr('dy', '0.35em')
     //   .text((d) => d.data.name)
-  }, [data, props.data, radius, w])
+  }, [
+    angleInterpolation,
+    arcGenerator,
+    arcs,
+    color,
+    data,
+    durations.entryAnimation,
+    innerRadiusInterpolation,
+    outerRadiusInterpolation,
+    props.data,
+    w,
+  ])
 
   return (
     <Box ref={ref}>
       <svg ref={svgRef} id="demo-pie-chart">
-        <g ref={pieRef} />
+        <g ref={pieRef}></g>
         {/* <g className="legend" /> */}
       </svg>
     </Box>
