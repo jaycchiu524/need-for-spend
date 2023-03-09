dab60e5dbf72625b7424aa4e6e2bdfdca8ecd52d
Jay Chiu <jaycchiu524@gmail.com>
2023-03-09 13:30:24 -0500

    ci: fix execSync

--- packages/server/tests/global/setup.js
+++ packages/server/tests/global/setup.js
@@ -34,13 +34,12 @@ module.exports = async () => {
     } catch (error) {
       console.error(error)
       throw error
+    } finally {
+      // ️️️✅ Best Practice: Use npm script for data seeding and migrations
+      execSync('yarn workspace server prisma generate && yarn workspace server prisma db push && yarn workspace server prisma migrate dev --name init', {
+        stdio: 'inherit',
+      })
     }
-
-
-    // ️️️✅ Best Practice: Use npm script for data seeding and migrations
-    execSync('yarn workspace server prisma generate && yarn workspace server prisma db push && yarn workspace server prisma migrate dev --name init', {
-      stdio: 'inherit',
-    })
   }
   console.timeEnd('global-setup')
 }
