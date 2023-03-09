b407b5ba6214a8645d71b78674844067ff056f99
Jay Chiu <jaycchiu524@gmail.com>
2023-03-09 12:55:46 -0500

    test: fix jest setup

--- packages/server/tests/global/setup.js
+++ packages/server/tests/global/setup.js
@@ -12,26 +12,32 @@ module.exports = async () => {
   if (!isDBReachable) {
     console.log('\n🐳 Starting docker-compose for testing...')
 
-    await dockerCompose.upAll({
-      // env: path.join(__dirname, '.env'),
-      cwd: path.join(__dirname),
-      log: true,
-    })
-
-    // wait for the mysql database to be ready
-    // sleep 1 is not enough -> 3
-    await dockerCompose.exec(
-      'db',
-      ['sh', '-c', `while ! mysqladmin ping -h localhost -uroot -uroot --silent; do
-      sleep 3
-      done`],
-      {
+    try {
+      await dockerCompose.upAll({
+        // env: path.join(__dirname, '.env'),
         cwd: path.join(__dirname),
-      },
-    )
+        log: true,
+      })
+
+      // wait for the mysql database to be ready
+      // sleep 1 is not enough -> 3
+      await dockerCompose.exec(
+        'db',
+        ['sh', '-c', `while ! mysqladmin ping -h localhost -uroot -uroot --silent; do
+        sleep 3
+        done`],
+        {
+          cwd: path.join(__dirname),
+        },
+      )
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+
 
     // ️️️✅ Best Practice: Use npm script for data seeding and migrations
-    execSync('yarn workspace prisma generate && yarn workspace prisma prisma db push && yarn workspace prisma prisma migrate dev --name init', {
+    execSync('yarn workspace server prisma generate && yarn workspace server prisma prisma db push && yarn workspace server prisma prisma migrate dev --name init', {
       stdio: 'inherit',
     })
   }
