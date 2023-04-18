91066d6f5277edfd6bbf7f859b48b4dfbee5671d
Jay Chiu <jaycchiu524@gmail.com>
2023-04-18 01:31:38 -0400

    fix: remove createAdmin

--- packages/server/prisma/seed.ts
+++ packages/server/prisma/seed.ts
@@ -1,10 +1,7 @@
 import debug from 'debug'
 
-import argon2 from 'argon2'
-
 import { prisma } from '@/configs/prismaClient'
 import { categoriesServices } from '@/plaid/categories/services'
-import { usersServices } from '@/users/users.services'
 
 // Database seeding happens in two ways with Prisma:
 // 1. manually with prisma db seed
@@ -14,28 +11,9 @@ const log = debug('prisma-seeding')
 
 const populateCategories = categoriesServices.syncPlaidCategories
 
-const createAdmin = async () => {
-  if (!process.env.ADMIN_PASSWORD) {
-    log('No admin password provided. Using default password')
-    throw new Error('No admin password provided. Using default password')
-  }
-
-  const encryptedPassword = await argon2.hash(process.env.ADMIN_PASSWORD)
-  await usersServices.createAdmin({
-    email: 'admin@nfs.com',
-    password: encryptedPassword,
-    firstName: 'Admin',
-    lastName: 'Admin',
-  })
-}
-
 const main = async () => {
   log('Seeding: Populating categories')
   await populateCategories()
-
-  log('Seeding: Creating admin')
-  console.log('🌱 Seeding: Creating admin')
-  await createAdmin()
 }
 
 main()
